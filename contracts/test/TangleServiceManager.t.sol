2b145ba4e66713c4e873848a335d7107fb1c3c67
Tjemmmic <mdtjemmes@proton.me>
2024-11-15 15:47:03 -0600

    fix!: using ecdsa contracts

--- contracts/test/TangleServiceManager.t.sol
+++ contracts/test/TangleServiceManager.t.sol
@@ -53,6 +53,7 @@ contract TangleServiceManagerTest is EigenlayerBase {
             address(_mailbox)
         );
         _tsm.initialize(address(this));
+        _tsm.setSlasher(slasher);
 
         IStrategy mockStrategy = IStrategy(address(0x1234));
         Quorum memory quorum = Quorum({strategies: new StrategyParams[](1)});
